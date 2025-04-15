8e661a13c5ed155643b473c18868057f8b863175
Fredson Chaves <fredsonchaves07@gmail.com>
2025-04-15 07:54:10 -0300

    feat: initial project

---
+++ src/main/java/github/fredsonchaves07/productmanager/ProductManagerApplication.java
@@ -0,0 +1,13 @@
+package github.fredsonchaves07.productmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/fredsonchaves07/productmanager/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.fredsonchaves07.productmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
